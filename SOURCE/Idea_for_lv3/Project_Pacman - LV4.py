cb1908e1c3320989124f839011cf43b5292190f0
leleeeee <huynhducle20000@gmail.com>
2020-08-16 12:34:07 +0800

    repair LV4

--- SOURCE/Idea_for_lv3/Project_Pacman - LV4.py
+++ SOURCE/Idea_for_lv3/Project_Pacman - LV4.py
@@ -90,24 +90,27 @@ def add_adjacent(matrix, weight, height):
     for i in range(1, height - 1):
         for j in range(1, weight - 1):
             temp = []
-            if matrix[i][j] == 1:
+            if matrix[i][j] == 1 or matrix[i][j] == 3:
                 continue
-            if matrix[i - 1][j] != 1:
+            if matrix[i - 1][j] != 1 or matrix[i - 1][j] == 3:
                 temp.append((i - 1, j))
-            if matrix[i + 1][j] != 1:
+            if matrix[i + 1][j] != 1 or matrix[i + 1][j] == 3:
                 temp.append((i + 1, j))
-            if matrix[i][j - 1] != 1:
+            if matrix[i][j - 1] != 1 or matrix[i][j - 1] == 3:
                 temp.append((i, j - 1))
-            if matrix[i][j + 1] != 1:
+            if matrix[i][j + 1] != 1 or matrix[i][j + 1] == 3:
                 temp.append((i, j + 1))
             dict[(i, j)] = temp
 
     return dict
 
-def lv3(matrix, dict, spawn, width, height):
+def lv4(matrix, dict, spawn, width, height):
 
     i = spawn[0]    #Lưu spawn thành 2 điểm
     j = spawn[1]
+	
+	temp_dict = {}
+	temp_dict = dict
 
     #Mục tiêu là đi nhiều nhất để tìm đồ ăn nên sẽ có điểm âm hơi lớn
     #Mỗi một node sẽ có một số lần đi qua nhất định để tránh việc đi lại nhiều lần, dồng thời cũng làm cho việc đi lùi khó khăn, duyệt không hết map
@@ -130,9 +133,6 @@ def lv3(matrix, dict, spawn, width, height):
                 if n == key[0] and m == key[1]:
                     step[n][m] = sum(1 for v in value if v)
             total += step[n][m]
-
-    number_adj_list = []
-    number_adj_list = step
 
     step[i][j] -= 1 #Điểm spawn giảm sẫn 1 bước đi
 
@@ -162,61 +162,40 @@ def lv3(matrix, dict, spawn, width, height):
         #Quái di chuyển bằng Heuristic tìm pacman
         for monster in monster_list:
             monster_path = []
-            monster_path = BFS(matrix, dict, (i, j), (monster[0], monster[1]))
+            monster_path = A_star(matrix, temp_dict, (monster[0], monster[1]), (i, j))
             monster_tuple = monster_path.pop(1)
             matrix[monster[0]][monster[1]] = 0
             matrix[monster_tuple[0]][monster_tuple[1]] = 3
-
-        #Biến vùng xung quanh monster thành tường
-        to_wall_list = []
-        for monster in monster_list:
-            for pos in monster:
-                if matrix[pos[0] + 1][pos[1]] == 0:
-                    matrix[pos[0] + 1][pos[1]] = 1
-                    to_wall_list.append((pos[0] + 1, pos[1]))
-
-                if matrix[pos[0]][pos[1] + 1] == 0:
-                    matrix[pos[0]][pos[1 + 1]] = 1
-                    to_wall_list.append((pos[0], pos[1] + 1))
-
-                if matrix[pos[0] + 1][pos[1] + 1] == 0:
-                    matrix[pos[0] + 1][pos[1] + 1] = 1
-                    to_wall_list.append((pos[0] + 1, pos[1] + 1))
-
-                if matrix[pos[0] + 1][pos[1] - 1] == 0:
-                    matrix[pos[0] + 1][pos[1] - 1] = 1
-                    to_wall_list.append((pos[0] + 1, pos[1] - 1))
-
-                if matrix[pos[0] - 1][pos[1] + 1] == 0:
-                    matrix[pos[0] - 1][pos[1] + 1] = 1
-                    to_wall_list.append((pos[0] - 1, pos[1] + 1))
-
-                if matrix[pos[0] - 1][pos[1]] == 0:
-                    matrix[pos[0] - 1][pos[1]] = 1
-                    to_wall_list.append((pos[0] - 1, pos[1]))
-
-                if matrix[pos[0] - 1][pos[1] - 1] == 0:
-                    matrix[pos[0] - 1][pos[1] - 1] = 1
-                    to_wall_list.append((pos[0] - 1, pos[1] - 1))
-
-                if matrix[pos[0]][pos[1] - 1] == 0:
-                    matrix[pos[0]][pos[1] - 1] = 1
-                    to_wall_list.append((pos[0], pos[1] - 1))
-
-        dict  = add_adjacent(matrix, width, height)
+		
+		temp_dict.clear()
+        temp_dict = add_adjacent(matrix, width, height)
+		
+		#Tạo danh sách các node có adj, values là số lượng adj
+		number_adj_list = []
+		for n in range(0, height):
+	        temp = []
+	        for m in range(0, width):
+	            temp.append(0)
+	        number_adj_list.append(temp)
+			
+		for n in range(0, height):
+	        for m in range(0, width):
+	            for key, value in temp_dict.items():
+	                if n == key[0] and m == key[1]:
+	                    number_adj_list[n][m] = sum(1 for v in value if v)
 
         #Tạo list các key và list các values của dictionary
         keys = []
-        for k in dict.keys():
+        for k in temp_dict.keys():
             keys.append(k)
         values = []
-        for k in dict.values():
+        for k in temp_dict.values():
             for o in k:
                 values.append(o)
 
         list_zone = []  #Tạo zone, vùng nhìn thấy của pacman, các elements trong zone là tuple tọa độ của caca1 node
-        for n in range(-4, 4):
-            for m in range(-4, 4):
+        for n in range(-3, 3):
+            for m in range(-3, 3):
                 if i + n < 0 or j + m < 0 or i + n >= height or j + m >= width:
                     continue
                 if matrix[i + n][j + m] != 1 and matrix[i + n][j + m] != 3:
@@ -226,7 +205,7 @@ def lv3(matrix, dict, spawn, width, height):
         for k in list_zone:
             path = []
             if matrix[k[0]][k[1]] == 2:
-                path = A_star(matrix, dict, (i, j), (k[0], k[1]))
+                path = A_star(matrix, temp_dict, (i, j), (k[0], k[1]))
                 if len(pre_path) < len(path) and len(pre_path) != 0:
                     continue
                 have_2 = True
@@ -248,13 +227,13 @@ def lv3(matrix, dict, spawn, width, height):
         else:   #Tìm đường theo một thừ tự nhất định khi không có thức ăn trong zone theo thứ tự node là phải, dưới, trên, trái
             for key in keys:
                 if (i, j) == key:
-                    if (i, j + 1) in values and step[i][j + 1] > 0 and (i, j + 1) != pre_position:
+                    if (i, j + 1) in values and step[i][j + 1] > 0 and ((i, j + 1) != pre_position or number_adjacent_list[i][j] == 1):
                         step[i][j + 1] -= 1
                         pre_position = (i, j)
                         j += 1
                         score -= 1
                         break
-                    if (i + 1, j) in values and step[i + 1][j] > 0  and (i + 1, j) != pre_position:
+                    if (i + 1, j) in values and step[i + 1][j] > 0  and ((i + 1, j) != pre_position or number_adjacent_list[i][j] == 1):
                         step[i + 1][j] -= 1
                         pre_position = (i, j)
                         i += 1
@@ -272,10 +251,6 @@ def lv3(matrix, dict, spawn, width, height):
                         j -= 1
                         score -= 1
                         break
-               
-        #Trả về đường sau khi quái di chuyển
-        for brick in to_wall_list:
-            matrix[brick[0]][brick[1]] = 0
 
     print((i, j))
     return score
